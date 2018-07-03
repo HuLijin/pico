9e469591bc9f89ae9ce1168e8ae0c03f2264399c
grrim <grimfw@gmail.com>
2018-07-03 15:41:31 +0200

    reorganize inside a MVProject

---
+++ plugins/pico-core.js
@@ -0,0 +1,14 @@
+//=============================================================================
+// pico-core.js
+//=============================================================================
+
+/*:
+ * @plugindesc Tool for Event Making.
+ * @author RME team
+ * @version 1.0.0
+ *
+ * This plugin add SelfVariables and some small operands !
+ * This code is released under MIT license.
+ */
+
+const message = 'foo/bar';
