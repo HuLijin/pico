e49c2f2e42053936389da116db3d44f8a62c5f36
grrim <grimfw@gmail.com>
2018-07-09 00:58:28 +0200

    Initialize "picture_show"

--- plugins/pico-picture.js
+++ plugins/pico-picture.js
@@ -1,12 +1,11 @@
 'use strict';
 
 /*:
- * @plugindesc An plugin example for πco
+ * @plugindesc Plugin Pictures for πco
  *
- * @author your-nickname <your-email>
+ * @author grm <grimfw@gmail.com>
  *
- * Describes the plugin
- * Describes the licenses
+ * License MIT
  */
 
 // Check dependencies betweens plugins
@@ -14,4 +13,15 @@ if (typeof π === 'undefined') throw 'Core is not installed';
 π.core.require([[π.core.version, '>= 1.0.0-dev']]);
 
 // The core of the script
-console.log('This script is valid!');
+π.picture = {};
+
+/**
+ * Show a picture
+ * @param {int} id the id of the picture
+ * @param {string} name the name of the picture
+ * @param {int} x the x-coord of the picture
+ * @param {int} y the y-coord of the picture
+ */
+π.picture.show = function(id, name, x, y) {
+  $gameScreen.showPicture(id, name, 0, x, y, 100, 100, 255, 0);
+};
